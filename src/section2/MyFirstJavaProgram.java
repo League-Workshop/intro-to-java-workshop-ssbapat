d088d1e2346f0be512c8f55f28d2f7264b9a9293
f*** you <beep@beep.com>
2019-04-16 14:25:13 -0700

    Day 2
    :smile:

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -8,8 +8,11 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-	
-		
-		
-	}
+  Robot r2d2 = new Robot();
+		 r2d2.penDown();
+		 r2d2.setSpeed(100);
+for(int i = 0; i < 4 ; i++) {
+		 r2d2.move(200);
+		 r2d2.turn(90);}
+	}		 
 }
